d0d68d77622ead3d708379ed75d4904d525406ea
Lokesh Wani <95117860+Lpwani@users.noreply.github.com>
2026-01-11 01:41:36 +0530

    Time: 26 ms (22.24%), Space: 46.7 MB (28.34%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -2,38 +2,45 @@ class Solution {
 
     int dp[][];
 
-    public int topDownDp(String s1, String s2, int i, int j, int sum, int dp[][]){
+    public int topDownDp(String s1, String s2, int i, int j){
         int m = s1.length();
         int n = s2.length();
 
-        
-
+        // Best Case
         if(i == m && j == n){
             return 0;
         }
-        else if(i == m){
-            dp[i][j+1] = (int)s2.charAt(j) + topDownDp(s1,s2,i,j+1,sum,dp); 
-            return dp[i][j+1];
-        }
-        else if(j == n){
-            dp[i+1][j] = (int)s1.charAt(i) + topDownDp(s1,s2,i+1,j,sum,dp);
-            return dp[i+1][j];
-        }
-        if(dp[i][j] != 0){
+
+        // Reuse
+        if(dp[i][j] != -1){
             return dp[i][j];
         }
-        else{
-            if(s1.charAt(i) == s2.charAt(j)){
-                return dp[i][j] = sum + topDownDp(s1,s2,i+1,j+1,sum,dp);
-            }
-            int choice1 = sum + (int)s1.charAt(i) + topDownDp(s1,s2,i+1,j,sum,dp);
-            int choice2 = sum + (int)s2.charAt(j) + topDownDp(s1,s2,i,j+1,sum,dp);
 
-            int min = Math.min(choice1, choice2);
-
-
-            return dp[i][j] = min;
+        // if s1 is exhausted
+        if(i == m){
+            return dp[i][j] = (int)s2.charAt(j) + topDownDp(s1,s2,i,j+1); 
         }
+
+        // if s2 is exhausted
+        if(j == n){
+            return dp[i][j] = (int)s1.charAt(i) + topDownDp(s1,s2,i+1,j);
+            
+        }
+        
+        // character matching
+        if(s1.charAt(i) == s2.charAt(j)){
+            return dp[i][j] = topDownDp(s1,s2,i+1,j+1);
+        }
+
+        // delete from either string
+        int deleteS1 = (int)s1.charAt(i) + topDownDp(s1,s2,i+1,j);
+        int deleteS2 = (int)s2.charAt(j) + topDownDp(s1,s2,i,j+1);
+
+        int min = Math.min(deleteS1, deleteS2);
+
+
+        return dp[i][j] = min;
+    
     }
 
     public int minimumDeleteSum(String s1, String s2) {
@@ -42,12 +49,17 @@ class Solution {
         int n = s2.length();
 
         dp = new int[m+1][n+1];
+
+        for(int i = 0; i < dp.length; i++){
+            Arrays.fill(dp[i],-1);
+        }
+
         // for(int i=0; i<m; i++){
         //     for(int j=0; j<n; j++){
         //         System.out.print(dp[i][j]+" ");
         //     }
         //     System.out.println();
         // }
-        return topDownDp(s1,s2,0,0,0,dp);    
+        return topDownDp(s1,s2,0,0);    
     }
 }
