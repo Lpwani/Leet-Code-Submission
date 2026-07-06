3266fc6cbd878b45ae9526d137aa48cdcda77523
Lokesh Wani <95117860+Lpwani@users.noreply.github.com>
2026-07-06 11:28:17 +0530

    Time: 0 ms (100%), Space: 44.1 MB (11.17%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,42 +1,74 @@
 class Solution {
     public int search(int[] nums, int target) {
+        
+        int currElm = nums[0];
+        boolean posneg = false;
+        int srtIdx = 0;
         int n = nums.length;
-        int low = 0;
-        int high = n - 1;
-
-        int idx = -1;
-
-        while(low <= high){
-            int mid = low + (high - low)/2;
-
-            if(target == nums[mid]){
-                idx = mid;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] >= nums[i-1]) continue;
+            else{
+                srtIdx = i;
                 break;
             }
-            
-            // left half is sorted
-            if(nums[mid] >= nums[low]){
-
-                if(target >= nums[low] && target <= nums[mid]){
-                    high = mid - 1;
-                }
-                else {
-                    low = mid + 1;
-                }
-            }
-
-            // if right half is sorted
-            else{
-                if(target >= nums[mid] && target <= nums[high]){
-                    low = mid + 1;
-                }
-                else{
-                    high = mid - 1;
-                }
-            }
-
         }
+        
+        int ans = -1;
+        for(int i = srtIdx; i < srtIdx + n; i++){
+            if(nums[i%n] == target) ans = i % n;
+        }
+        
+        return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+    
+        
+        
+        
+        
+        // int n = nums.length;
+        // int low = 0;
+        // int high = n - 1;
 
-        return idx;
+        // int idx = -1;
+
+        // while(low <= high){
+        //     int mid = low + (high - low)/2;
+
+        //     if(target == nums[mid]){
+        //         idx = mid;
+        //         break;
+        //     }
+            
+        //     // left half is sorted
+        //     if(nums[mid] >= nums[low]){
+
+        //         if(target >= nums[low] && target <= nums[mid]){
+        //             high = mid - 1;
+        //         }
+        //         else {
+        //             low = mid + 1;
+        //         }
+        //     }
+
+        //     // if right half is sorted
+        //     else{
+        //         if(target >= nums[mid] && target <= nums[high]){
+        //             low = mid + 1;
+        //         }
+        //         else{
+        //             high = mid - 1;
+        //         }
+        //     }
+
+        // }
+
+        // return idx;
     }
 }
