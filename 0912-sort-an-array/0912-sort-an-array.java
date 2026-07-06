5b9c4411d03eb721871c03225e5ae05cee1e2b24
Lokesh Wani <95117860+Lpwani@users.noreply.github.com>
2026-07-06 17:11:22 +0530

    Time: 43 ms (19.25%), Space: 86.5 MB (32.44%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    public void heapify(int nums[], int n, int i){
+
+        int left = i*2 + 1;
+        int right = i*2 + 2;
+        int largestIdx = i;
+
+        if(left < n && nums[left] > nums[i]) largestIdx = left;
+        if(right < n && nums[right] > nums[largestIdx]) largestIdx = right;
+
+        if(largestIdx != i){
+            // swap nums[i] and nums[largestIdx]
+            int temp = nums[i];
+            nums[i] = nums[largestIdx];
+            nums[largestIdx] = temp;
+
+            heapify(nums, n, largestIdx);
+        }
+
+    }
+
+    public void buildHeap(int nums[]){
+        int n = nums.length;
+
+        for(int i = (n-1)/2; i >= 0; i--){
+            heapify(nums,n, i);
+        }
+    } 
+    public int[] sortArray(int[] nums) {
+        
+        int n = nums.length;
+        // int ans[] = new int[n];
+
+        // int heapArr[] = buildHeap(nums);
+        buildHeap(nums);
+
+        int currPrevIdx = n - 1;
+        int corrIdx = 0;
+
+        for(int i = currPrevIdx; i >= 0; i--){
+            // swap nums[0] and nums[i]
+            int temp = nums[0];
+            nums[0] = nums[i];
+            nums[i] = temp;
+
+            heapify(nums,i, 0);
+        }
+
+        return nums;
+    }
+}
