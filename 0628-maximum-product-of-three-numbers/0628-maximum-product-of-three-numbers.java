8fb01e951178a3c6e1da8c360d1612c58466d6d4
Lokesh Wani <95117860+Lpwani@users.noreply.github.com>
2026-07-26 20:23:13 +0530

    Time: 17 ms (10.31%), Space: 48.1 MB (57.6%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,21 +1,48 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        // observation based optimized approach
-        // Time complexity = O(nlogn)
+        // observation based single pass optimized approach
+        // 5 magic number approach
+        // Time complexity = O(n)
         // Space Complexity = O(1)
 
-        int ans = Integer.MIN_VALUE;
         int n = nums.length;
+        int mx1 = Integer.MIN_VALUE, mx2 = Integer.MIN_VALUE, mx3 = Integer.MIN_VALUE; 
+        int mn1 = Integer.MAX_VALUE, mn2 = Integer.MAX_VALUE;
+
+        for(int i = 0; i < n; i++){
+            int curr = nums[i];
+
+            if(curr > mx1){
+                mx3 = mx2;
+                mx2 = mx1;
+                mx1 = curr;
+            }
+            else if(curr > mx2){
+                mx3 = mx2;
+                mx2 = curr;
+            }
+            else if (curr > mx3){
+                mx3 = curr;
+            }
+
+            if(curr < mn1){
+                mn2 = mn1;
+                mn1 = curr;
+            }
+            else if(curr < mn2){
+                mn2 = curr;
+            }
+        }
 
         Arrays.sort(nums);
 
-        // case 1 : all three greatest number
-        int case1Mul = nums[n-1] * nums[n-2] * nums[n-3];
+        // case 1 : three greatest number
+        int case1Mul = mx1 * mx2 * mx3;
 
         // case 2 : 2 smallest negative number, one greatest positive number
-        int case2Mul = nums[0] * nums[1] * nums[n-1]; 
+        int case2Mul = mn1 * mn2 * mx1; 
 
-        ans = Math.max(case1Mul, case2Mul);
+        int ans = Math.max(case1Mul, case2Mul);
 
         return ans;
 
