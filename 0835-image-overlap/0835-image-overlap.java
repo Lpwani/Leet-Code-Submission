12081692beaab483bb06a3ff309dd98677ec8ade
Lokesh Wani <95117860+Lpwani@users.noreply.github.com>
2026-09-14 01:46:09 +0530

    Time: 416 ms (5.32%), Space: 48.6 MB (5.7%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,24 +1,58 @@
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
+
+        // Time complexity : O(n^4)
+        // Space complexity : O(n)
+
+        HashMap<String,Integer> strOverlap = new HashMap<>();
+
         int n = img1.length;
-        // collect every coordinate that holds a 1
-        List<int[]> A = new ArrayList<>();
-        List<int[]> B = new ArrayList<>();
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (img1[i][j] == 1) A.add(new int[]{i, j});
-                if (img2[i][j] == 1) B.add(new int[]{i, j});
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(img1[i][j] == 1){
+                    // StringBuilder sb = new StringBuilder();
+                    for(int a = 0; a < n; a++){
+                        for(int b = 0; b < n; b++){
+                            if(img2[a][b] == 1){
+                                StringBuilder sb = new StringBuilder();
+                                int nums2row = a;
+                                int nums2col = b;
+                                while(nums2row-i > 0){
+                                    sb.append('d');
+                                    nums2row--;
+                                }
+                                while(nums2row-i < 0){
+                                    sb.append('u');
+                                    nums2row++;
+                                }
+                                while(nums2col-j > 0){
+                                    sb.append('r');
+                                    nums2col--;
+                                }
+                                while(nums2col-j < 0){
+                                    sb.append('l');
+                                    nums2col++;
+                                }
+
+                                strOverlap.put(sb.toString(), strOverlap.getOrDefault(sb.toString(),0)+1);
+                            }
+                        }
+                    }
+                }
             }
         }
-        int[][] cnt = new int[2 * n][2 * n];
-        int best = 0;
-        for (int[] a : A) {
-            for (int[] b : B) {
-                int dx = b[0] - a[0] + n;
-                int dy = b[1] - a[1] + n;
-                best = Math.max(best, ++cnt[dx][dy]);
-            }
+
+        for(Map.Entry<String,Integer> entry : strOverlap.entrySet()){
+            String key = entry.getKey();
+            int val = entry.getValue();
+
+            ans = Math.max(ans, val);
         }
-        return best;
+
+
+        return ans;
     }
 }
